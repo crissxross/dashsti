46aa05c486da3ee4a9558a5e6d1cd9f9534a213b
Christine Wilks <crissxross@gmail.com>
2017-06-28 13:55:09 +0100

    ok functions for P A D tweening hsl Saturation & Lightness in emoviz2

--- src/app/dashboard/emoviz2/emoviz2.component.ts
+++ src/app/dashboard/emoviz2/emoviz2.component.ts
@@ -15,10 +15,10 @@ import { TweenMax, TimelineMax, Power0 } from 'gsap';
       <svg>
         <svg:rect #bg class="bg" width="400" height="333"/>
         <svg:ellipse #gP cx="100" cy="80" rx="100" ry="80"/>
-        <svg:ellipse #gA cx="140" cy="90" rx="100" ry="80"/>
-        <svg:ellipse #gD cx="180" cy="100" rx="100" ry="80"/>
-        <svg:ellipse #rP cx="220" cy="100" rx="100" ry="80"/>
-        <svg:ellipse #rA cx="260" cy="90" rx="100" ry="80"/>
+        <svg:ellipse #gA cx="100" cy="80" rx="100" ry="80"/>
+        <svg:ellipse #gD cx="100" cy="80" rx="100" ry="80"/>
+        <svg:ellipse #rP cx="300" cy="80" rx="100" ry="80"/>
+        <svg:ellipse #rA cx="300" cy="80" rx="100" ry="80"/>
         <svg:ellipse #rD cx="300" cy="80" rx="100" ry="80"/>
         <!-- NOTE: transforms & opacity are the most performant things to animate
         GSAP shorthand for the transform properties: x, y, z, scale, rotation -->
@@ -71,57 +71,65 @@ export class Emoviz2Component implements OnInit, OnDestroy {
     const rD = this.rD.nativeElement;
     // set default PAD 000 values of SVG elements to animate
     TweenMax.set(bg, { fill: 'hsl(137, 30%, 10%)' });
-    TweenMax.set(gP, { fill: 'hsl(137, 50%, 50%)', opacity: 0.5 });
-    TweenMax.set(gA, { fill: 'hsl(137, 50%, 50%)', opacity: 0.5 });
-    TweenMax.set(gD, { fill: 'hsl(137, 50%, 50%)', opacity: 0.5 });
-    TweenMax.set(rP, { fill: 'hsl(0, 50%, 50%)', opacity: 0.5 });
-    TweenMax.set(rA, { fill: 'hsl(0, 50%, 50%)', opacity: 0.5 });
-    TweenMax.set(rD, { fill: 'hsl(0, 50%, 50%)', opacity: 0.5 });
+    TweenMax.set(gP, { fill: 'hsl(137, 50%, 50%)', opacity: 0.5, y: -20 });
+    TweenMax.set(gA, { fill: 'hsl(137, 50%, 50%)', opacity: 0.5, y: 90 });
+    TweenMax.set(gD, { fill: 'hsl(137, 50%, 50%)', opacity: 0.5, y: 200 });
+    TweenMax.set(rP, { fill: 'hsl(0, 50%, 50%)', opacity: 0.5, y: -20 });
+    TweenMax.set(rA, { fill: 'hsl(0, 50%, 50%)', opacity: 0.5, y: 90 });
+    TweenMax.set(rD, { fill: 'hsl(0, 50%, 50%)', opacity: 0.5, y: 200 });
+    // Note: GSAP shorthand for transform properties: x, y, z, scale, rotation
 
-//  P: brightness & saturation
+//  P: joint positive function of saturation & brightness (brightness has stronger influence)
     this.pProgress = this.pValue$
-      // returns a positive integer in steps of 5 from 0 to 100
-      .map(v => Math.round((v + 1) * 50))
+      // returns an integer from -100 to 100 (in steps of 10)
+      .map(v => Math.round(v * 100))
       .subscribe(v => {
-        console.log('P v: ', v);
-        TweenMax.to(gP, 0.5, { fill: `hsl(137, ${v}%, ${v}%)` });
-        TweenMax.to(rP, 0.5, { fill: `hsl(0, ${v}%, ${v}%)` });
+        const S = 50 + v / 2.5; // ranges from 10 to 90
+        const L = 50 + v / 2.1; // ranges from 2.38 to 97.62
+        TweenMax.to(gP, 0.5, { fill: `hsl(137, ${S}%, ${L}%)` });
+        TweenMax.to(rP, 0.5, { fill: `hsl(0, ${S}%, ${L}%)` });
+        console.log('P v: ', v, ' S: ', S, ' L: ', L);
       });
 
-//  A: brightness & saturation ???
+//  A: as Arousal increases, saturation increases strongly & brightness decreases
     this.aProgress = this.aValue$
-      // returns a positive integer from 0 to 50
-      .map(v => Math.round((v + 1) * 25))
+      // returns an integer from -100 to 100 (in steps of 10)
+      .map(v => Math.round(v * 100))
       .subscribe(v => {
-        console.log('A v: ', v);
-        TweenMax.to(gA, 0.5, { fill: `hsl(137, ${v * 2}%, ${v}%)` });
-        TweenMax.to(rA, 0.5, { fill: `hsl(0, ${v * 2}%, ${v}%)` });
+        const S = 50 + v / 2; // ranges from 0 to 100
+        const L = 50 - v / 3; // ranges from 83.333 to 16.666
+        TweenMax.to(gA, 0.5, { fill: `hsl(137, ${S}%, ${L}%)` });
+        TweenMax.to(rA, 0.5, { fill: `hsl(0, ${S}%, ${L}%)` });
+        console.log('A v: ', v, ' S: ', S, ' L: ', L);
       });
 
-//  D: brightness & saturation ??? - needs to be inverse effect of P on brightness !!!
+//  D: as Dominance increases, saturation increases & brightness decreases strongly
     this.dProgress = this.dValue$
-      // returns a positive integer from 0 to 20
-      .map(v => Math.round((v + 1) * 10))
+      // returns an integer from -100 to 100 (in steps of 10)
+      .map(v => Math.round(v * 100))
       .subscribe(v => {
-        console.log('A v: ', v);
-        TweenMax.to(gD, 0.5, { fill: `hsl(137, ${v}%, ${v}%)` });
-        TweenMax.to(rD, 0.5, { fill: `hsl(0, ${v}%, ${v}%)` });
+        const S = 50 + v / 2.5; // ranges from 10 to 90
+        const L = 50 - v / 2.1; // ranges from 97.62 to 2.38
+        TweenMax.to(gD, 0.5, { fill: `hsl(137, ${S}%, ${L}%)` });
+        TweenMax.to(rD, 0.5, { fill: `hsl(0, ${S}%, ${L}%)` });
+        console.log('D v: ', v, ' S: ', S, ' L: ', L);
       });
 
 
   // NOTES
     this.pNote = 'P: (hsl) saturation (S) & brightness (L) vary';
-    // because "Pleasure was simply a joint positive function of color brightness and saturation" (Valdez & Mehrabian, 1994)
+    // because "Pleasure was simply a joint positive function of color brightness and saturation"
     this.aNote = 'A: (hsl) saturation (S) varies';
     // because arousal increases with more saturation
     this.dNote = 'D: (hsl) brightness (L) varies';
-    // because "dominance decreases sharply with increases in colour brightness" (Valdez & Mehrabian, 1994)
+    // because "dominance decreases sharply with increases in colour brightness"
   }
+  // Quotes from 'Effects of Color on Emotions' (Valdez and Mehrabian, 1994)
 
   ngOnDestroy() {
     this.pProgress.unsubscribe();
-    // this.aProgress.unsubscribe();
-    // this.dProgress.unsubscribe();
+    this.aProgress.unsubscribe();
+    this.dProgress.unsubscribe();
     console.log('emoviz2 destroyed');
   }
 
