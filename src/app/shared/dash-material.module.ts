85747a0f00f6de0adec217c8ed24e3bfaac2efe0
Christine Wilks <crissxross@gmail.com>
2019-05-30 13:23:21 +0100

    updates to @angular/material & @angular/cdk 8.0.0

--- src/app/shared/dash-material.module.ts
+++ src/app/shared/dash-material.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatIconModule,
-  MatToolbarModule,
-  MatSidenavModule,
-  MatListModule,
-  MatSliderModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
